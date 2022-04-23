d2285633c6ed8a3e80a79ccfb459caed0143eca4
nabilk11 <khan.nabiln11@gmail.com>
2022-04-23 10:16:53 -0400

    installed Pillow for image uploading, completed views and url router reg for products

--- backend/api/inventory/urls.py
+++ backend/api/inventory/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.CategoryViewSet)
+router.register(r'', views.CategoryView)
+router.register(r'', views.ProductView)
 
 
 urlpatterns = [
--- backend/api/inventory/views.py
+++ backend/api/inventory/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import CategorySerializer, ProductSerializer
+from .models import Category, Product
 
 # Create your views here.
 
 # Category View Set
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by('title')
     serializer_class = CategorySerializer
 
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('name')
+    serializer_class = ProductSerializer
