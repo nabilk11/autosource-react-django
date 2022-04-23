99b7eabfda011ad5035f2607bdde73ec39c6089b
nabilk11 <khan.nabiln11@gmail.com>
2022-04-23 11:28:20 -0400

    installed react-bootstrap for styling, basic styling for navbar and footer completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
+import { Container } from 'react-bootstrap';
+import NavHeader from './components/NavHeader';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      Sneaker Source App
+      <NavHeader />
+      <main className='py-4' >
+        <Container>
+          <h1>SneakerSource | Welcome</h1> 
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
