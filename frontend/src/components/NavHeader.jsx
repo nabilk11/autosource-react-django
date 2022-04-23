ccdc361f53a6cc605e67eb29acfa8b96ade50a8e
nabilk11 <khan.nabiln11@gmail.com>
2022-04-23 14:44:15 -0400

    added bootstrap Link Containers for all nav links

--- frontend/src/components/NavHeader.jsx
+++ frontend/src/components/NavHeader.jsx
@@ -1,31 +1,58 @@
 import React from 'react';
 import { Container, Navbar, Nav, Row, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 
 function NavHeader() {
   return (
     <header>
        <Navbar collapseOnSelect variant='dark' bg="dark" expand="lg">
         <Container>
-            <Navbar.Brand href="/">SneakerSource</Navbar.Brand>
+          <LinkContainer to={"/"}>
+            <Navbar.Brand >SneakerSource</Navbar.Brand>
+          </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/register">Register</Nav.Link>
-                <Nav.Link href="/login">Login</Nav.Link>
+            <LinkContainer to={"/"}>
+              <Nav.Link >Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to={"/register"}>
+              <Nav.Link >Register</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to={"/register"}>
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+
                 <strong>
                   <NavDropdown renderMenuOnMount={true} title="Sneakers" id="basic-nav-dropdown"> 
-                  <NavDropdown.Item href="#jordan">Air Jordan</NavDropdown.Item>
-                  <NavDropdown.Item href="#NikeSB">Nike SB</NavDropdown.Item>
-                  <NavDropdown.Item href="#Nike">Nike</NavDropdown.Item>
-                  <NavDropdown.Item href="#Yeezy">Yeezy</NavDropdown.Item>
-                  <NavDropdown.Item href="#Other">Other</NavDropdown.Item>
+                  <LinkContainer to={"/jordan"}>
+                    <NavDropdown.Item >Air Jordan</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to={"/nikesb"}>
+                    <NavDropdown.Item >Nike SB</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to={"/yeezy"}>
+                    <NavDropdown.Item >Yeezy</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to={"/nike"}>
+                    <NavDropdown.Item >Nike</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to={"/other"}>
+                    <NavDropdown.Item >Other</NavDropdown.Item>
+                  </LinkContainer>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#products">View All Products</NavDropdown.Item>
+                  <LinkContainer to={"/products"} >
+                    <NavDropdown.Item>View All Products</NavDropdown.Item>
+                  </LinkContainer>
                   </NavDropdown>
                 </strong>
-                <Nav.Link href="#Cart">View Cart</Nav.Link>
-                <Nav.Link href="#contact">Contact Us</Nav.Link>
+                <LinkContainer to={"/cart"}>
+                  <Nav.Link>Cart</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to={"/contact"}>
+                  <Nav.Link>Contact Us</Nav.Link>
+                </LinkContainer>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
