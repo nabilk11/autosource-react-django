91049e9a67eb84c082eca01b782e8fe1d242cc44
nabilk11 <khan.nabiln11@gmail.com>
2022-04-23 09:44:21 -0400

    created product serializers and model, registered to admin

--- backend/api/inventory/serializers.py
+++ backend/api/inventory/serializers.py
@@ -1,8 +1,15 @@
 from dataclasses import fields
-from .models import Category
+from .models import Category, Product
 from rest_framework import serializers
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+        
