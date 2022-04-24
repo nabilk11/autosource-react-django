fbabfe5a5200bb8f23d76210f7649bdee069187f
nabilk11 <khan.nabiln11@gmail.com>
2022-04-24 14:00:04 -0400

    added OrderedProducts model - linking orders to products

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,7 @@
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import CharField
 
 # Create your models here.
 
@@ -68,7 +70,19 @@ class Order(models.Model):
     class Meta:
         ordering = ['delivered']
 
-       
+
+class OrderedProducts(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+  
 
 
 
