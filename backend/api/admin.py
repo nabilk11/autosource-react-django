94be780d97aa5301eb43b61fdf6cb9b8726f5f44
nabilk11 <khan.nabiln11@gmail.com>
2022-04-24 15:00:48 -0400

    added shippinginfo model, with 3day shipping date save method, and migrated

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Order, Product, OrderedProducts
+from .models import Category, Order, Product, OrderedProducts, ShippingInfo
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderedProducts)
+admin.site.register(ShippingInfo)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
-from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import CharField
+import datetime
 
 # Create your models here.
 
@@ -22,11 +21,11 @@ class Category(models.Model):
     description = models.TextField(max_length=255, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-class Meta:
-    ordering = ['title']
+    class Meta:
+        ordering = ['title']
 
 
 # Products Model
@@ -61,7 +60,7 @@ class Order(models.Model):
     deliveryTime = models.BooleanField(default=False)
     delivered = models.BooleanField(default=False)
     createdAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     
 
     def __str__(self):
@@ -70,7 +69,7 @@ class Order(models.Model):
     class Meta:
         ordering = ['delivered']
 
-
+# Ordered Products Model
 class OrderedProducts(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
@@ -82,7 +81,27 @@ class OrderedProducts(models.Model):
         return self.name
 
 
-  
+# Shipping Information
+class ShippingInfo(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, blank=True, null=True)
+    shippingCost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    address = models.CharField(max_length=155, null=True, blank=True)
+    city = models.CharField(max_length=155, null=True, blank=True)
+    state = models.CharField(max_length=155, null=True, blank=True)
+    zipCode = models.CharField(max_length=10, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    shippingDate = models.DateField()
 
+    def __str__(self):
+        return self.order
+
+    class Meta:
+        ordering = ['shippingDate']
+
+# standard 3 days shipping
+    def save(self, *args, **kwargs):
+        if self.shippingDate is None:
+            self.shippingDate = self.createdAt.date() + datetime.timedelta(days=3)
+        super(ShippingInfo, self).save(*args, **kwargs)   
 
 
