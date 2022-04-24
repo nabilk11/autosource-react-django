75c175dc5dc487267439887d19ee98c77affa2d4
nabilk11 <khan.nabiln11@gmail.com>
2022-04-23 20:43:55 -0400

    refactored useEffect hook on product page with api call to backend

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Button, Card, Col, Row, Image } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
@@ -11,11 +12,12 @@ const id = useParams().id;
 
 // useEffect hook to setSneaker
 useEffect(() => {
-    const fetchSneaker = () => {
-        const res = sneakers.find((s)=> 
-            s._id === id
-        )
-        setSneaker(res)
+    const fetchSneaker = async () => {
+        const res = await axios.get('/api/products/'+id)
+        // sneakers.find((s)=> 
+        //     s._id === id
+        // )
+        setSneaker(res.data)
     }
     fetchSneaker();
 }, [id])
