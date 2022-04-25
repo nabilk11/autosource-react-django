520f96a4bcd504a19c56715541f6ec8706825472
nabilk11 <khan.nabiln11@gmail.com>
2022-04-25 02:39:59 -0400

    created full cart ui, with empty remove handler and dispatch for updating quantities

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col, Image, Form, Button, Card } from "react-bootstrap";
+import { Row, Col, Image, ListGroupItem, Form, Button, Card, Alert, ListGroup } from "react-bootstrap";
 import { addToCart } from '../redux/actions/cartActions';
 
 export const CartPage = () => {
@@ -11,10 +11,12 @@ const cartProdId = useParams().id;
 const location = useLocation()
 // dispatch hook
 const dispatch = useDispatch()
-
 // find qty from params
 const qty = location.search ? Number(location.search.split('=')[1]) : 1 
-
+// useSelector hook to obtain new cart state
+const cart = useSelector(state => state.cart)
+const { cartProds } = cart
+console.log(cartProds)
 
 
 useEffect(()=> {
@@ -23,9 +25,55 @@ useEffect(()=> {
   }
 }, [dispatch, cartProdId, qty])
 
+
+const removeProd = (id) => {
+
+}
   return (
     <div>
-        CartPage
+        <h1>Shopping Cart</h1>
+        <Row>
+          <Col md={12} >
+            {cartProds.length === 0 
+            ? <Alert>
+              Your Cart is Currently Empty!
+            </Alert>
+            : <Card>
+              {cartProds.map(p => (
+                <ListGroup>
+                  <ListGroupItem className='mt-5' >
+                <Row>
+                  <Col md={2} >
+                    <Image width={'70px'} src={p.images} />
+                  </Col>
+                  <Col md={3} >
+                    <Link to={`/product/${p.product}`}>{p.name}</Link>
+                  </Col>
+                  <Col md={2} >
+                    $ {p.price}
+                  </Col>
+                  <Col md={2} >
+                  <Form.Control value={p.stock} as="select" onChange={(e)=> dispatch(addToCart(p.product, e.target.value))} >
+                        {[...Array(p.count).keys()].map((p) => (
+                            <option key={p+2} value={p+1} >{p+1}</option>
+                        ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={1} >
+                    <Button onClick={()=> removeProd(p.product) } >
+                          Remove
+                    </Button>
+                  </Col>
+                </Row>
+                </ListGroupItem>
+                </ListGroup>
+              ))}
+              </Card>}
+          </Col>
+          <Col>
+          
+          </Col>
+        </Row>
 
     </div>
   )
