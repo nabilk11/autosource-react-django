380655feb69edea6edbaa4d1840652f8f4ebce5b
nabilk11 <khan.nabiln11@gmail.com>
2022-04-26 21:52:25 -0400

    added loginActions, api call to retreive token and set to local storage

---
+++ frontend/src/redux/actions/userActions.js
@@ -0,0 +1,36 @@
+import { 
+    LOGIN_ERROR, 
+    LOGIN_START, 
+    LOGIN_SUCCESS, 
+    LOGOUT } from "../reducers/userReducer";
+import axios from 'axios';
+
+
+export const loginCall = (email, password) => async (dispatch) => {
+
+    try {
+        dispatch({
+            type: LOGIN_START
+        })
+        const headers = {'Content-Type': 'application/json'}
+
+        const res = await axios.post('/api/users/login/', {
+            'username': email, 'password': password,
+        },
+        {headers: headers}
+        )
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+        localStorage.setItem('userToken', JSON.stringify(res.data.access))
+        // add error handling
+    } catch (err) {
+        dispatch({
+        type: LOGIN_ERROR,
+        payload: err, 
+    })
+        
+    }
+    
+}
