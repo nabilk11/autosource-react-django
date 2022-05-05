7b2575129de88a1ee9410e4d19a69fc0d5be4336
nabilk11 <khan.nabiln11@gmail.com>
2022-05-04 22:57:47 -0400

    added category header conditional rendering

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -7,21 +7,44 @@ import Product from '../components/Product';
 export const CategoryPage = () => {
 
     const catId = useParams().id
-    const [products, setProducts] = useState()
+    const [products, setProducts] = useState([])
 
 
     useEffect(()=> {
         const fetchCategoryProducts = async () => {
             const res = await axios.get(`/api/category/${catId}/`)
             setProducts(res.data)
+            console.log(res.data)
         }
         fetchCategoryProducts()
     }, [catId])
 
   return (
     <Container>
-        <h2 className='mt-4'>All Products <br />
-        <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       {products.length > 0 ? <>{ products[0].category == 1 ? <><h2 className='mt-4'>All Jordans <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>
+       : products[0].category === 2 ? <><h2 className='mt-4'>All NikeSBs <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>
+       : products[0].category === 3 ? <><h2 className='mt-4'>All Nikes <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>
+       : products[0].category === 4 ? <><h2 className='mt-4'>All Yeezys <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>
+       : products[0].category === 5 ? <><h2 className='mt-4'>Other Brands <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>
+       : products[0].category === 6 ? <><h2 className='mt-4'>Men's Apparel <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>
+       : <><h2 className='mt-4'>All Yeezys <br /> 
+       <small className='text-muted mb-3'>The very best only at SneakerSource!</small> </h2> <br />
+       </>}
+       </>
+       : <h1>No Current Products in this Category!</h1>}
+        
 
         {products && <Row>
             {products.map(s => (
